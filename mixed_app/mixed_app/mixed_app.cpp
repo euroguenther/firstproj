122adadd57aa73a6300f1ea29ae4665283b6c7bd
orockstroh <orockstroh@PC04146578>
2017-02-13 12:21:39 +0100

    kindergeburtstag

--- mixed_app/mixed_app/mixed_app.cpp
+++ mixed_app/mixed_app/mixed_app.cpp
@@ -2,12 +2,28 @@
 //
 
 #include<stdio.h>
+#include<stdlib.h>
+#include"firstProjFun.h"
+
 
 
 int main()
 {
+	int a = 0;
+	system("COLOR");
 
-	int a = 10;
-    return 0;
-}
+	printf("+++++++++Auswahl+++++++++++");
+	printf("Bitte Auswahl treffen\n");
+	printf("K = Kindergeburtstag\n");
+	printf("S = Scan berechnen\n");
+	scanf("%c", &a);
+
+	a == 115 || a == 83 ? fScan() : fKinder();
+
+
+	return 0;
+	}
+
+
+
 
